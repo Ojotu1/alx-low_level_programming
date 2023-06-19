dd59cd40e30cfbebe0a07144c5a51238d67cb8a0
Ojotu1 <ephraimhojotu@gmail.com>
2023-06-19 13:25:03 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  *main - print the last digit of the number stored in the variable n.
  *
@@ -6,14 +8,16 @@
  */
 int main(void)
 {
-	
-	char low;
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
 	putchar(low);
-	
-	for (low = 'A'; low <= 'Z'; low++)
-	putchar(low);
+	}
 	putchar('\n');
 
 	return (0);
