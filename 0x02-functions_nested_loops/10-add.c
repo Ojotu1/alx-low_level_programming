fc9ef8ba8e05cf709f9fe00b98e7e8bc17a46e17
Ojotu1 <ephraimhojotu@gmail.com>
2023-06-21 08:19:04 -0700

    Task 9 to 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/** 
+ * add - Adds two integers 
+ * @n1: First integer to be added 
+ * @n2: second integer to be added. 
+ * Return: The result of the addition 
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -18,8 +18,9 @@ void jack_bauer(void);
 
 void times_table(void);
 
-int add(int);
+int add(int, int);
 
 void print_to_98(int n);
 
 void print_times_table(int n);
+
